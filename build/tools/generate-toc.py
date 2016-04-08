a61b4498b467c3c50f693810f44bd9f7588c6349
thboileau <thboileau@gmail.com>
2016-04-08 16:32:05 +0200

    Allowed to generated some DHC files

--- build/tools/generate-toc.py
+++ build/tools/generate-toc.py
@@ -13,10 +13,16 @@ def main():
                                                  'All files & folders must begin with xx_name.')
     parser.add_argument('--path', '-p',  type=str, help='Path to scan. If not provided takes current path.')
     parser.add_argument('--markdown', '-m',  action='store_true', help='If provided, return a markdown file.')
+    parser.add_argument('--gwtsources', '-gs',  action='store_true', help='If provided, return a list of GWT sources.')
+    parser.add_argument('--gwtpanel', '-gp',  action='store_true', help='If provided, return a list of GWT sources.')
+    parser.add_argument('--gwtjsonindex', '-gj',  action='store_true', help='If provided, return a list of GWT sources.')
 
     args = parser.parse_args()
     path = args.path
     markdown = args.markdown
+    gwtsources = args.gwtsources
+    gwtpanel = args.gwtpanel
+    gwtjsonindex = args.gwtjsonindex
 
     if path is None:
         path = os.getcwd()
@@ -26,10 +32,16 @@ def main():
 
     obj = {'toc': document}
 
-    if not markdown:
-        print yaml.dump(obj, default_flow_style=False)
-    else:
+    if markdown:
         convert_to_markdown(document)
+    elif gwtsources:
+        convert_to_gwt_sources(document)
+    elif gwtpanel:
+        convert_to_gwt_panel(document)
+    elif gwtjsonindex:
+        convert_to_gwt_json_index(document)
+    else:
+        print yaml.dump(obj, default_flow_style=False)
 
 
 def scan_dir(path, document, path_to_display, link_url):
@@ -99,18 +111,62 @@ def add_markdown_file(path, document, yml_path, path_to_display, link_url):
 def convert_to_markdown(document):
     indentation = ''
     for line in document:
-        scan_line(line, indentation)
+        print_line_as_markdown(line, indentation)
 
-
-def scan_line(line, indentation):
+def print_line_as_markdown(line, indentation):
     if 'dir' in line.keys():
         # Is directory
         print '%s* %s' % (indentation, line['title'])
         for item in line['items']:
-            scan_line(item, indentation + '  ')
+            print_line_as_markdown(item, indentation + '  ')
     elif 'file' in line.keys():
         # Is file
         print '%s* [%s](%s)' % (indentation, line['title'], line['link_url'])
 
+def convert_to_gwt_sources(document):
+    indentation = '    '
+    for line in document:
+        print_line_as_gwt_sources(line, indentation)
+
+def print_line_as_gwt_sources(line, indentation):
+    if 'dir' in line.keys():
+        for item in line['items']:
+            print_line_as_gwt_sources(item, indentation)
+    elif 'file' in line.keys():
+        # Is file
+        print '%s@Source("%s")' % (indentation, line['link_url'][1:])
+        print '%sTextResource %s();' % (indentation, line['url'].replace("/", "_").replace("-", "_"))
+
+def convert_to_gwt_panel(document):
+    indentation = '    '
+    for line in document:
+        print_line_as_gwt_panel(line, indentation, "/")
+def print_line_as_gwt_panel(line, indentation, dir):
+    if 'dir' in line.keys():
+        # Is directory
+        print '%s%s.addItem("%s", "%s", null)' % (indentation, indentation, line['title'], dir + line['id'])
+        for item in line['items']:
+            print_line_as_gwt_panel(item, indentation, dir + line['id'] + "/")
+    elif 'file' in line.keys():
+        # Is file
+        print '%s%s.addItem("%s", "%s", toHTML(DocsResources.INSTANCE.%s()))' % (indentation, indentation, line['title'], line['url'], line['url'].replace("/", "_").replace("-", "_"))
+
+def convert_to_gwt_json_index(document):
+    indentation = '    '
+    listHelpPages = []
+    for line in document:
+        print_line_as_gwt_json_index(listHelpPages, line, "/")
+    print '[\n%s\n]' % (",\n".join(listHelpPages))
+
+def print_line_as_gwt_json_index(listHelpPages, line, dir):
+    if 'dir' in line.keys():
+        # Is directory
+        listHelpPages.append('{"dir": true, "title":"%s", "path": "%s"}' % (line['title'], dir + line['id']))
+        for item in line['items']:
+            print_line_as_gwt_json_index(listHelpPages, item, dir + line['id'] + "/")
+    elif 'file' in line.keys():
+        # Is file
+        listHelpPages.append('{"dir": false, "title":"%s", "path": "%s"}' % (line['title'], line['url']))
+ 
 if __name__ == "__main__":
     main()
